ed621bb6c86ec7342df2f0529cfa64136df6d9b3
loxyion <kersten.lorenz@partner.ionity.eu>
2024-06-24 23:05:53 +0200

    First content with Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import {defineConfig} from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), preact()],
+  integrations: [mdx(), preact(), tailwind()]
 });
